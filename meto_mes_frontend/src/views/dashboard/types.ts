bc7a53796e1608c6c90b18d6f08641fee41d39b5
adtkb34 <116409989+adtkb34@users.noreply.github.com>
2025-12-04 09:03:28 +0800

    Add work order filter to dashboard

--- meto_mes_frontend/src/views/dashboard/types.ts
+++ meto_mes_frontend/src/views/dashboard/types.ts
@@ -14,6 +14,7 @@ export interface FilterState {
   product: string[];
   origin: ProductOrigin | null;
   processCode: string | null;
+  workOrderCode: string | null;
 }
 
 export interface ProcessMetric {
